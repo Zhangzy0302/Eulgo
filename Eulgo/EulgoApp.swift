8dd210610bf555ef6e8ce0c7945b8c5970920efc
zzy <2218847957@qq.com>
2026-06-04 10:49:34 +0800

    Initial Commit

---
+++ Eulgo/EulgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulgoApp.swift
+//  Eulgo
+//
+//  Created by yangyang on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct EulgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
